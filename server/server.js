b1164f831381f2b3299fbdbc811b55f568cf5a0c
sidkalla04 <141704442+sidkalla04@users.noreply.github.com>
2026-05-28 16:21:29 +0530

    cors updated

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ connectDB();
 const app = express();
 
 app.use(morgan('dev'));
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
